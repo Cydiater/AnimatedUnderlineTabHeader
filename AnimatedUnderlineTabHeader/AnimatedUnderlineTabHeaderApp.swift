01c9ad4c81d04ae0855c2cf9128d8b9789002a73
Cydiater <cydiater@gmail.com>
2024-06-30 18:46:24 +0800

    Initial Commit

---
+++ AnimatedUnderlineTabHeader/AnimatedUnderlineTabHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedUnderlineTabHeaderApp.swift
+//  AnimatedUnderlineTabHeader
+//
+//  Created by Cydiater on 30/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedUnderlineTabHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
